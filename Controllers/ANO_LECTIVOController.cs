92de97ce48780e2f3c097159eb6487b9b72efa88
mrgomeza <mrgomeza962@gmail.com>
2022-10-15 23:12:55 -0500

    Cuarto Commit

---
+++ Controllers/ANO_LECTIVOController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Prueba;
+
+namespace Prueba.Controllers
+{
+    public class ANO_LECTIVOController : Controller
+    {
+        private GatsuGradesv8Entities db = new GatsuGradesv8Entities();
+
+        // GET: ANO_LECTIVO
+        public ActionResult Index()
+        {
+            return View(db.ANO_LECTIVO.ToList());
+        }
+
+        public List<string> Anios()
+        {
+            List<string> alist = new List<string>();
+            ANO_LECTIVO atemp=new ANO_LECTIVO();
+            List<ANO_LECTIVO> anios = db.ANO_LECTIVO.ToList();
+            for (int i = 0; i < anios.Count(); i++)
+            {
+                alist.Add(anios[i].ANO_DESCRIP.ToString()); ;
+            }
+            return alist;
+        }
+        // GET: ANO_LECTIVO/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ANO_LECTIVO aNO_LECTIVO = db.ANO_LECTIVO.Find(id);
+            if (aNO_LECTIVO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aNO_LECTIVO);
+        }
+
+        // GET: ANO_LECTIVO/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ANO_LECTIVO/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_ANO,ANO_DESCRIP")] ANO_LECTIVO aNO_LECTIVO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ANO_LECTIVO.Add(aNO_LECTIVO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(aNO_LECTIVO);
+        }
+
+        // GET: ANO_LECTIVO/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ANO_LECTIVO aNO_LECTIVO = db.ANO_LECTIVO.Find(id);
+            if (aNO_LECTIVO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aNO_LECTIVO);
+        }
+
+        // POST: ANO_LECTIVO/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_ANO,ANO_DESCRIP")] ANO_LECTIVO aNO_LECTIVO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aNO_LECTIVO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aNO_LECTIVO);
+        }
+
+        // GET: ANO_LECTIVO/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ANO_LECTIVO aNO_LECTIVO = db.ANO_LECTIVO.Find(id);
+            if (aNO_LECTIVO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aNO_LECTIVO);
+        }
+
+        // POST: ANO_LECTIVO/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ANO_LECTIVO aNO_LECTIVO = db.ANO_LECTIVO.Find(id);
+            db.ANO_LECTIVO.Remove(aNO_LECTIVO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ www/js/llenadoCBHor.js
@@ -0,0 +1,47 @@
+﻿
+
+function FillComboAnio() {
+    var combo = document.getElementById("calectivo");
+    var option = document.createElement('option');
+    // añadir el elemento option y sus valores
+    combo.options.add(option, 0);
+    combo.options[0].value = "valor";
+    combo.options[0].innerText = "Texto";
+}
+function llenaComboAnios() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://localhost:44332/ANO_LECTIVO/Anios',
+        success: function (data, textStatus, jqXHR) {
+            $.each(data, function (key, value) {
+                $(
+                    
+                ).appendTo('#Jugadores');
+            }
+            );
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alter('Estatus: ' + textStatus + '(' + errorThrown + ')');
+        }
+    });
+}
+
+document.addEventListener("load", llenaComboAnios);
+document.getElementById('#grad').Attributes.Add("onChange", "metodoJavascript(this);");
+combo = document.getElementById('#grad');
+function metodoJavascript(combo) {
+    var valor = combo.options[combo.selectedIndex].value;
+    alert(valor);
+}
+
+alert(document.getElementById('#grad').value);
+
+
+   //resto del script si lo hay
+
+ if(ViewBag.Message != ""){
+
+    alert('@ViewBag.Message');
+
+ }
+
