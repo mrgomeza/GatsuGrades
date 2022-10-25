c3ae10e6b9c00c963c3b1f43bf30fe80a5e4ccc6
FernandoPuce <lfrodriguezr@puce.edu.ec>
2022-10-24 20:56:53 -0500

    Prueba

--- Prueba/Modelo.Designer.cs
+++ Prueba/Modelo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Laptop\Documents\6to_Semestre\DesarrolloSI\GatsuGrades-Todas las versiones\Prueba\Modelo.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\ferna\source\repos\GatsuGrades\Repo\Prueba\Modelo.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
